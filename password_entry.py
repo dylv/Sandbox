6a7683f2892c90c10fc03f72a0e2697ab8cc17cf
dylv <dylan.valmadre@my.jcu.edu>
2019-03-22 12:37:08 +1000

    Updated code: Inserted hand-written password_check into the py. file.

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,14 @@
 """Dylan Valmadre"""
+
+min_length = 10
+
+valid_pw = False
+while not valid_pw:
+    password = input(str("Enter password: "))
+    if len(password) < min_length:
+        valid_pw = False
+        print("Password must contain at least 10 digits.")
+    else:
+        valid_pw = True
+        for char in password:
+            print("*", end=" ")
